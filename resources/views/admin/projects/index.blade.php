56904056b328eb368ddec4c9010fef55bed3dad2
nicola1397 <nicola1397@outlook.it>
2024-04-02 18:14:29 +0200

    feat: admin index view

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<section>
+<div class="container py-4">
+
+<div class="row">
+    @foreach($projects as $project)
+    <div class="col">
+        <div class="card">
+            <div class="card-header">
+                <h2>{{$project['name']}}</h2>
+            </div>
+            <div class="card-body">
+                <ul>
+                    <li><b>date: </b>{{$project['date']}}</li>
+                    <li><b>description: </b>{{$project['description']}}</li>
+                    <li><b>image: </b>{{$project['image']}}</li>
+                </ul>
+            </div>
+            <!-- <div class="card-footer">
+            </div> -->
+        </div>
+    </div>
+    
+    @endforeach
+</div>
+
+</div>
+</section>@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\Guest\DashboardController as GuestDashboardController;
 use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use Illuminate\Support\Facades\Route;
@@ -28,6 +29,7 @@ Route::middleware('auth')
     Route::get('/dashboard', [AdminDashboardController::class, 'index'])
       ->name('dashboard');
 
+    Route::resource('projects', ProjectController::class);
   });
 
 require __DIR__ . '/auth.php';
